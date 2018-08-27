18951ab9fd06d9a3d459cae3824bb6196f504403
jeremy ramos <jeremy@jeremyramos.com>
2018-08-27 12:02:28 -0700

    intial setup

---
+++ DbConnector/Adapter/DbAdapter.cs
@@ -0,0 +1,131 @@
+﻿using DbConnector.Tools;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbConnector.Adapter
+{
+    public class DbAdapter
+    {
+        public IDbCommand DbCommand { get; private set; }
+        public IDbConnection DbConnection { get; private set; }
+        private const int _timeout = 5000;
+
+        public DbAdapter(IDbCommand dbCommand, IDbConnection dbConnection)
+        {
+            DbCommand = dbCommand;
+            DbConnection = dbConnection;
+        }
+
+        public void LoadFromDataSets(string storedProcedure, 
+            Action<IDataReader, int> dataMapper, IDbDataParameter[] parameters = null)
+        {
+            if (string.IsNullOrEmpty(storedProcedure))
+                throw new ArgumentException("Missing stored procedure");
+            int result = 0;
+            using (IDbConnection conn = DbConnection)
+            using (IDbCommand cmd = DbCommand)
+            {
+                if(conn.State != ConnectionState.Open) { conn.Open(); }
+                cmd.CommandTimeout = _timeout;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = storedProcedure;
+                cmd.Connection = conn;
+                if (parameters != null)
+                    foreach (IDbDataParameter param in parameters)
+                        cmd.Parameters.Add(param);
+
+                IDataReader reader = cmd.ExecuteReader();
+                while (true)
+                {
+                    while (reader.Read())
+                    {
+                        dataMapper?.Invoke(reader, result);
+                    }
+                    result++;
+                    if (reader.IsClosed || !reader.NextResult())
+                        break;
+
+                    if (result > 10)
+                        throw new Exception("Too many results sets returned");
+                }
+            }
+        }
+
+        public IEnumerable<T> LoadObject<T>(string storedProcedure,
+            IDbDataParameter[] parameters = null) where T: class
+        {
+            List<T> items = new List<T>();
+            using (IDbConnection conn = DbConnection)
+            using (IDbCommand cmd = DbCommand)
+            {
+                if(conn.State != ConnectionState.Open) { conn.Open(); }
+                cmd.CommandTimeout = _timeout;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = storedProcedure;
+                cmd.Connection = conn;
+                if(parameters != null)
+                {
+                    foreach (IDbDataParameter param in parameters)
+                        cmd.Parameters.Add(param);
+                }
+
+                IDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    items.Add(DataMapper<T>.Instance.MapToObject(reader));
+                }
+            }
+            return items;
+        }
+
+        public int ExecuteNonQuery(string storedProcedure, 
+            IDbDataParameter[] parameters,
+            Action<IDbDataParameter[]> returnParameters = null)
+        {
+            using (IDbConnection conn = DbConnection)
+            using(IDbCommand cmd = DbCommand)
+            {
+                if(conn.State != ConnectionState.Open) { conn.Open(); }
+                cmd.CommandTimeout = _timeout;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = storedProcedure;
+                cmd.Connection = conn;
+                if(parameters != null)
+                {
+                    foreach (IDbDataParameter param in parameters)
+                        cmd.Parameters.Add(param);
+                }
+
+                int returnVal = cmd.ExecuteNonQuery();
+                returnParameters?.Invoke(parameters);
+
+                return returnVal;
+            }
+        }
+
+        public object ExecuteScalar(string storedProcedure, 
+            IDbDataParameter[] parameters = null)
+        {
+            using (IDbConnection conn = DbConnection)
+            using (IDbCommand cmd = DbCommand)
+            {
+                if (conn.State != ConnectionState.Open) { conn.Open(); }
+                cmd.CommandTimeout = _timeout;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = storedProcedure;
+                cmd.Connection = conn;
+                if (parameters != null)
+                {
+                    foreach (IDbDataParameter param in parameters)
+                        cmd.Parameters.Add(param);
+                }
+
+                return cmd.ExecuteScalar();
+            }
+        }
+    }
+}
---
+++ DbConnector/Tools/DataHelper.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbConnector.Tools
+{
+    // C# ext class 
+    public static class DataHelper
+    {
+        public static string AddXToString(this string input)
+        {
+            return input + "X";
+        }
+
+        public static string AddYToString(string input)
+        {
+            return input +"Y";
+        }
+
+        public static int Add10ToNumber(this int input)
+        {
+            return input + 10;
+        }
+
+        public static double GetDouble(this DataRow dr, string columnName)
+        {
+            double.TryParse(dr[columnName].ToString(), out double dbl);
+            return dbl;
+        }
+
+        public static double GetDouble(this IDataReader reader, string columnName)
+        {
+            double.TryParse(reader[columnName].ToString(), out double dbl);
+            return dbl;
+        }
+
+        public static T GetParamValue<T>(this IDbDataParameter[] dbParams, string paramName)
+        {
+            foreach(IDbDataParameter param in dbParams)
+            {
+                if (param.ParameterName.ToLower() == paramName.ToLower())
+                    return (T)Convert.ChangeType(param.Value, typeof(T));
+            }
+            return default(T);
+        }
+    }
+
+    public class Test
+    {
+        public void Tester()
+        {
+            int num = 20;
+            num = num.Add10ToNumber();
+            string _num = "test";
+            DataHelper.AddYToString(_num);
+
+            
+        }
+    }
+
+}
